e223ef3749aa720cf90d8f7adbd23643dccf64db
Ned_Chen <Congjia.Chen21@imperial.ac.uk>
2022-04-19 12:01:26 +0100

    19th Apr: Revise the Process.MetaData.R

--- Project/code/Process.MetaData.R
+++ Project/code/Process.MetaData.R
@@ -2,18 +2,41 @@
 
 if(!require("dplyr")) {install.packages("dplyr")}
 require(dplyr)
+require(tidyverse)
 
 #previous metadata
 
 df_old= read.table("map_UKBB.txt",sep = "\t",header = T)
 
-df_old = df_old[(df_old$Species != ""),] 
-df_old = na.omit(df_old)
+df_old = df_old[(df_old$Sample.ID != ""),]
 
-df.old.revised  <- df_old %>% mutate(BarcodeSequence_old=paste0(substr(df_old$Index1i7Sequence,1,7),substr(df_old$Index2i5Sequence,1,7)),
-                     LinkerPrimerSequence_old=Linker1PrimerSequence,
-                     sample_name=Sample.ID) %>% dplyr::select("sample_name","BarcodeSequence_old","LinkerPrimerSequence_old","CollectionSite","Species","Infection","Infection.intensity","Description")
+df.old.revised  <- df_old %>% mutate(BarcodeSequence=paste0(substr(df_old$Index1i7Sequence,1,7),substr(df_old$Index2i5Sequence,1,7)),
+                     LinkerPrimerSequence=Linker1PrimerSequence,
+                     sample_name=Sample.ID) %>% dplyr::select("sample_name","BarcodeSequence","LinkerPrimerSequence","CollectionSite","Species","Infection","Infection.intensity","Description")  %>% 
+                      replace_na(list(CollectionSite = "Blank",Species = "Blank",Infection = "Blank",Infection.intensity = "Blank"))
   
+
+
+write.table(df.old.revised,file = "../results/1.Quality_Control/Demultiplexing/sample-metadata.tsv",row.names = F,sep="\t")
+
+write.table(df.old.revised,file = "./sample-metadata.tsv",row.names = F,sep="\t")
+
+#to discover the pattern of barcode sequence
+#zcat lane1_Undetermined_L001_R2_1.fastq.gz | grep '^@' | cut -d: -f10 | sort | uniq -c | sort - -k1rn | head -n120 > result.txt
+
+df_top120Barcode = read.delim("../16sRaw/result.txt",header = F) %>% 
+  mutate(repeats = extract_numeric(V1),
+  BarcodeSequence = str_sub(V1,-14)) %>%
+  select("repeats","BarcodeSequence" ) %>% 
+ left_join(df.old.revised,by = c("BarcodeSequence"))
+
+write.table(df_top120Barcode,file = "./Top120Barcode.tsv",row.names = F,sep="\t")
+
+
+
+
+
+
 #updated metadata
 df_new = read.table("micro_map.txt",sep="\t",header = T)
 df.new.revised <-  df_new %>% mutate(BarcodeSequence=paste0(substr(df_new$Index1i7Sequence,1,7),substr(df_new$Index2i5Sequence,1,7)),
